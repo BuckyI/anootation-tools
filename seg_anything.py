9df228bc48ae77c8609dd537f8f629b97e74d802
WangHanlin <2047430257@qq.com>
2023-04-14 21:31:58 +0800

    🦄 refactor(seg_anything): 重构、优化成函数形式
    之前是脚本，现在变成函数可以被调用

--- seg_anything.py
+++ seg_anything.py
@@ -1,9 +1,7 @@
 from segment_anything import sam_model_registry, SamPredictor
 import numpy as np
-import torch
 import matplotlib.pyplot as plt
 import cv2
-import sys
 
 
 def show_points(coords, labels, ax, marker_size=375):
@@ -25,53 +23,60 @@ def show_mask(mask, ax, random_color=False):
     ax.imshow(mask_image)
 
 
-# 导入图片
-image = cv2.imread('assets/Snipaste_2023-04-13_13-56-00.jpg')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+def load_image(path: str):
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
 
-# 加载模型
-sam_checkpoint = "assets/sam_vit_b_01ec64.pth"
-model_type = "vit_b"
-device = "cuda"
-sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-sam.to(device=device)
-predictor = SamPredictor(sam)
-predictor.set_image(image)
 
-# 获得 mask
-logits = None
-input_points = []
-input_labels = []
-masks = None
-while True:
-    # 展示图片结果
-    # fig, ax = plt.subplots()
+def get_mask(image: np.ndarray):
+    # 加载模型
+    sam_checkpoint = "assets/sam_vit_b_01ec64.pth"
+    model_type = "vit_b"
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    sam.to(device="cuda")
+    predictor = SamPredictor(sam)
+    predictor.set_image(image)
+
+    # 获得 mask
+    input_points = []
+    input_labels = []
+    logits = None
+    mask = None
+    while True:
+        # 展示图片结果
+        plt.imshow(image)
+        plt.axis('off')
+        if mask is not None:
+            show_mask(mask, plt.gca())
+            show_points(np.array(input_points),
+                        np.array(input_labels), plt.gca())
+            plt.draw()
+        # 读取输入的点
+        points = plt.ginput(n=2, timeout=-1)
+        if not points:
+            print("finished")
+            break
+        input_points.extend(points)
+        input_labels.extend([1, 0])
+
+        # get mask
+        mask, __, logits = predictor.predict(
+            point_coords=np.array(input_points),
+            point_labels=np.array(input_labels),
+            mask_input=logits,
+            multimask_output=False,
+        )
+    return mask
+
+
+if __name__ == '__main__':
+    image = load_image('assets/Snipaste_2023-04-13_13-56-00.jpg')
+    masks = get_mask(image)
+
+    # 展示结果
+    plt.close()
     plt.imshow(image)
-    plt.axis('off')
-    # ax.imshow(image)
-    if masks is not None:
-        show_mask(masks, plt.gca())
-        show_points(np.array(input_points), np.array(input_labels), plt.gca())
-        plt.draw()
-    # 读取输入的点
-    points = plt.ginput(n=2, timeout=-1)
-    if not points:
-        print("finished")
-        break
-    input_points.extend(points)
-    input_labels.extend([1, 0])
-
-    # get mask
-    masks, __, logits = predictor.predict(
-        point_coords=np.array(input_points),
-        point_labels=np.array(input_labels),
-        mask_input=logits,
-        multimask_output=False,
-    )
-
-# 展示结果
-plt.imshow(image)
-plt.axis('off')
-show_mask(masks, plt.gca())
-show_points(np.array(input_points), np.array(input_labels), plt.gca())
-plt.show()
+    plt.axis('on')
+    show_mask(masks, plt.gca())
+    plt.show()
