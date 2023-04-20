47899994d2f45474ef2cac8d02de4db9acc67b89
WangHanlin <2047430257@qq.com>
2023-04-20 17:23:50 +0800

    ✨ feat(coco_utils): mask2coco annotation

--- coco_utils.py
+++ coco_utils.py
@@ -19,3 +19,23 @@ def bounding_box_from_mask(mask: np.ndarray):
     convex_hull = cv2.convexHull(np.array(all_contours))
     x, y, w, h = cv2.boundingRect(convex_hull)
     return x, y, w, h
+
+
+def parse_mask_to_coco(image_id, anno_id, image_mask, category_id):
+    x, y, width, height = bounding_box_from_mask(image_mask)
+
+    fortran_binary_mask = np.asfortranarray(image_mask)
+    encoded_mask = encode(fortran_binary_mask)
+    encoded_mask['counts'] = str(encoded_mask['counts'], 'utf-8')
+
+    annotation = {
+        "id": anno_id,
+        "image_id": image_id,
+        "category_id": category_id,
+        "bbox": [float(x), float(y), float(width), float(height)],
+        "area": float(width * height),
+        "iscrowd": 0,
+        "segmentation": encoded_mask,
+    }
+    return annotation
+
