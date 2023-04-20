0bc6486309eae63c5049c7e84677473c7afae79d
WangHanlin <2047430257@qq.com>
2023-04-20 22:45:52 +0800

    ✨ feat(seg_anything): 增加连续标注功能

--- seg_anything.py
+++ seg_anything.py
@@ -3,6 +3,10 @@ from segment_anything import sam_model_registry, SamPredictor
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import coco_utils
+from pycocotools.coco import COCO
+import pickle
+import os
 
 
 def show_points(coords, labels, ax, marker_size=375):
@@ -30,13 +34,15 @@ def load_image(path: str):
     return image
 
 
-def get_mask(image: np.ndarray):
+def get_mask(image: np.ndarray, predictor: SamPredictor = None):
+    if predictor is None:
     # 加载模型
     sam_checkpoint = "assets/sam_vit_b_01ec64.pth"
     model_type = "vit_b"
     sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
     sam.to(device="cuda")
     predictor = SamPredictor(sam)
+
     predictor.set_image(image)
 
     # 获得 mask
@@ -51,7 +57,7 @@ def get_mask(image: np.ndarray):
         if mask is not None:
             show_mask(mask, plt.gca())
             show_points(np.array(input_points),
-                        np.array(input_labels), plt.gca())
+                        np.array(input_labels), plt.gca(), marker_size=80)
             plt.draw()
         # 读取输入的点
         points = plt.ginput(n=2, timeout=-1)
@@ -71,13 +77,58 @@ def get_mask(image: np.ndarray):
     return mask[0]
 
 
-if __name__ == '__main__':
-    image = load_image('assets/Snipaste_2023-04-13_13-56-00.jpg')
-    masks = get_mask(image)
+class Annotator:
+    def __init__(self, image_dir: str, annotation: str):
+        # 加载模型
+        sam_checkpoint = "assets/sam_vit_b_01ec64.pth"
+        model_type = "vit_b"
+        sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+        sam.to(device="cuda")
+        self.predictor = SamPredictor(sam)
 
-    # 展示结果
+        # annotation settings
+        self.annotation = COCO(annotation)
+        self.image_dir = image_dir
+
+        self.current_img = None
+
+    def mask_path(self, filename: str):
+        return os.path.join(self.image_dir, 'masks', os.path.basename(filename)).replace("\\", "/")
+
+    def annotate_image(self, filename: str):
+        # def click_save(event):
+        #     if event.button == 1:  # 左键点击保存
+        #         coco_utils.mask2file(mask, self.mask_path(filename))
+        #     elif event.button == 3:  # 右键点击取消
+        #         plt.close()
+        #         self.annotate_image(filename)
+
+        image = load_image(os.path.join(self.image_dir, filename))
+        mask = get_mask(image, predictor=self.predictor)
+        # confirm
     plt.close()
     plt.imshow(image)
     plt.axis('on')
-    show_mask(masks, plt.gca())
+        show_mask(mask, plt.gca())
+        # plt.connect('button_press_event', click_save)
     plt.show()
+        coco_utils.mask2file(mask, self.mask_path(filename))
+
+    def search_unannotated_images(self):
+        for filename in os.listdir(self.image_dir):
+            if os.path.isdir(os.path.join(self.image_dir, filename)):
+                continue
+            if os.path.exists(self.mask_path(filename)):
+                continue
+            else:
+                yield filename
+
+    def annotate_images(self):
+        for filename in self.search_unannotated_images():
+            self.annotate_image(filename)
+
+
+if __name__ == '__main__':
+    s = Annotator("images", "annotation.json").annotate_images()
+
+
