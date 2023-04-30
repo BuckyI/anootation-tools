5c36712fb0c07ff92c66302e784e14d6837cb3ba
WangHanlin <2047430257@qq.com>
2023-04-30 17:58:14 +0800

    🎈 perf(show_mask): change mask color

--- seg_anything.py
+++ seg_anything.py
@@ -42,7 +42,9 @@ def show_mask(mask, ax, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
     else:
-        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+        # color = np.array([30 / 255, 144 / 255, 255 / 255, 0.4]) # 蓝色
+        color = np.array([129 / 255, 20 / 255, 184 / 255, 0.5])  # 紫色
+        # color = np.array([239 / 255, 83 / 255, 80 / 255, 0.5])  # 红色
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
